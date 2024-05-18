8c5b06b91a0b1249b0805843cb9bc86726622f12
Tarek <egypty3@gmail.com>
2024-05-18 20:26:17 +0300

    updated the column of the home_sections with a listview.builder

--- lib/widgets/subwidgets/home_section_tab.dart
+++ lib/widgets/subwidgets/home_section_tab.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:the_app/utils/appcolors.dart';
 import 'package:the_app/widgets/apptext.dart';
+import 'package:the_app/widgets/news_widget.dart';
 import 'package:webfeed_plus/domain/rss_feed.dart';
 import 'package:http/http.dart' as http;
 
@@ -19,11 +21,10 @@ class _HomeSectionTabState extends State<HomeSectionTab> {
     try {
       var response = await http.get(Uri.parse(
           "https://news.google.com/rss/headlines/section/topic/${widget.topic.toUpperCase()}?ceid=US:EN&hl=en&gl=US"));
-      if (response.statusCode == 200)
-        {
-         // print(response.body);
-          feed = RssFeed.parse(response.body);
-        }
+      if (response.statusCode == 200) {
+        // print(response.body);
+        feed = RssFeed.parse(response.body);
+      }
     } catch (e) {
       print('Error loading RSS feed: $e');
     }
@@ -55,6 +56,20 @@ class _HomeSectionTabState extends State<HomeSectionTab> {
               ),
             )
           ],
+        ),
+        SizedBox(
+          child: (feed == null)
+              ? Center(child: CupertinoActivityIndicator())
+              : ListView.builder(itemBuilder: (context, index) {
+                  var item = feed!.items![index];
+
+                  return NewsWidget(
+                      title: item.title ?? '',
+                      subtitle: item.description ?? '',
+                      publishDate: item.pubDate.toString() ?? '',
+                      author: item.source!.value ?? '',
+                      link: item.link ?? '');
+                }),
         )
       ],
     );
